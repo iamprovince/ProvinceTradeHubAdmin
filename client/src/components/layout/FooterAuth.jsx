f1e7feab30a081ed1e2aeba0e8847012509fbb7e
theGenesisio <genesisioinfo@gmail.com>
2025-02-01 21:42:23 +0100

    footer adjustment

--- client/src/components/layout/FooterAuth.jsx
+++ client/src/components/layout/FooterAuth.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='absolute bottom-0 left-0 w-full bg-transparent text-sm text-text-light text-center py-2 flex justify-center flex-col items-center space-y-0.5'>
+    <footer className='absolute bottom-0 left-0 w-full bg-transparent text-sm text-text-light text-center py-2 justify-center flex-col items-center space-y-0.5 hidden lg:flex'>
       © {new Date().getFullYear()} Province Trade Hub
       <br /> All rights reserved.
     </footer>
